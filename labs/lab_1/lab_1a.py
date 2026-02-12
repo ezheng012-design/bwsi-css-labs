e6ceedb6e638798747351b63641b84d5ae69b753
ezheng012-design <ezheng012@gmail.com>
2026-02-12 10:15:24 -0500

    added introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Eric" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi! My name is Eric Zheng, and I'm currently a junior from Fort Lee High School, NJ! I'm super passionate about mathematics and its applications! For my extracurriculars, I'm on the XC and track and field team and enjoy learning math beyond the school curriculum!")
 if __name__ == "__main__":
     main()
